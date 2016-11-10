c35bb83c27d31e5012d12c925c41edc2853b7c40
Phillip Schichtel <phillip.public@schich.tel>
2016-11-10 22:44:33 +0100

    Changed the conversion script to create lower-quality MP3

--- scripts/audio/mpeg/audio/x-flac.cmd
+++ scripts/audio/mpeg/audio/x-flac.cmd
@@ -3,9 +3,5 @@
 set "source=%~1"
 set "target=%~2"
 
-if "%MP3_BITRATE%"=="" (
-    set MP3_BITRATE=248
-)
-
 :: explicit output format "-f mp3" is required as the conversion target will be a temp file.
-ffmpeg -y -i "%source%" -ab "%MP3_BITRATE%k" -map_metadata 0 -id3v2_version 3 -f mp3 "%target%"
+ffmpeg -y -i %FFMPEG_OPTIONS% "%source%"  -map_metadata 0 -id3v2_version 3 -f mp3 %FFMPEG_OUT_OPTIONS% "%target%"
