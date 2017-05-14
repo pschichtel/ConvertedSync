be615a26099420faee289303704f79aecef90e95
Phillip Schichtel <phillip.public@schich.tel>
2017-05-14 05:37:02 +0200

    fixed conversion scripts

--- scripts/audio/mpeg/audio/x-flac.cmd
+++ scripts/audio/mpeg/audio/x-flac.cmd
@@ -4,4 +4,4 @@ set "source=%~1"
 set "target=%~2"
 
 :: explicit output format "-f mp3" is required as the conversion target will be a temp file.
-ffmpeg -y -i %FFMPEG_OPTIONS% "%source%"  -map_metadata 0 -id3v2_version 3 -f mp3 %FFMPEG_OUT_OPTIONS% "%target%"
+ffmpeg -y -i "%source%" %FFMPEG_OPTIONS% -q:a 5 -map_metadata 0 -id3v2_version 3 -f mp3 "%target%"
