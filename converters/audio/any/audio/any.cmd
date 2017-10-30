203e315db1693fe01401247c5a98e0226cad565b
Phillip Schichtel <phillip.public@schich.tel>
2017-10-30 17:07:53 +0100

    remove old conversion scripts

--- converters/audio/any/audio/any.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set "source=%~1"
-set "target=%~2"
-
-:: explicit output format like "-f mp3" is required as the conversion target will be a temp file.
-ffmpeg -y -i "%source%" %FFMPEG_OPTIONS% "%target%"
